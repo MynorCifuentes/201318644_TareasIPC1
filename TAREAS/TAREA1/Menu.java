23e0cf314f6d706dc0a102a2b1f46291c7c80d90
Mynor Cifuentes <mynorcifuentes@ieee.org>
2020-08-07 23:50:17 -0600

    opcion 3 lista

--- TAREAS/TAREA1/Menu.java
+++ TAREAS/TAREA1/Menu.java
@@ -1,16 +1,11 @@
 
 import java.util.Scanner;
 
-
-
-
-
-public class Menu{
-    public static void main(String[] args){
+public class Menu {
+    public static void main(String[] args) {
         int opcion;
         Scanner entrada = new Scanner(System.in);
-       
-       
+
         do {
             System.out.println("Seleccione una opcion");
             System.out.println("[1] Indicar si un numero es par ");
@@ -20,39 +15,57 @@ public class Menu{
             System.out.println("[5] Salir ");
             opcion = entrada.nextInt();
 
-            switch(opcion){
-                case 1: 
-                System.out.println("Ingrese un numero");
-                int numero = entrada.nextInt();
-                if(numero % 2 == 0){
-                    System.out.println("El numero: "+numero+" es un numero par");
-                }else{
-                    System.out.println("El numero: "+numero+" es un numero impar");
-                }
-                break;
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese un numero");
+                    int numero = entrada.nextInt();
+                    if (numero % 2 == 0) {
+                        System.out.println("El numero: " + numero + " es un numero par");
+                    } else {
+                        System.out.println("El numero: " + numero + " es un numero impar");
+                    }
+                    break;
 
+                case 2:
+                    System.out.println("Ingrese numero base");
+                    int base = entrada.nextInt();
+                    System.out.println("Ingrese final");
+                    int nfinal = entrada.nextInt();
+                    for (int i = 1; i <= nfinal; i++) {
 
-                case 2: 
-                System.out.println("Ingrese numero base");
-                int base = entrada.nextInt();
-                System.out.println("Ingrese final");
-                int nfinal = entrada.nextInt();
-                for(int i = 1; i<= nfinal; i++){
+                        System.out.println(base + "x" + i + "=" + i * base);
+                    }
+                    break;
 
-                    System.out.println(base+"x"+i+"="+i*base);
-                }
-                break;
-                case 3: 
-                System.out.println("Opcion 3");
-                break;
-                case 4: 
-                System.out.println("Opcion 4");
-                break;
-                case 5: 
-                System.out.println("Salir");
-                break;
+                case 3:
+                    boolean esPrimo;
+                    System.out.println("Ingrese numero inicial");
+                    int num_inicial = entrada.nextInt();
+                    System.out.println("Ingrese numero final");
+                    int num_final = entrada.nextInt();
+                    System.out.println("----------------------------");
+                    for (int i = num_inicial; i <= num_final; i++) {
+                        esPrimo=true;
+                        for (int j = 2; j < i; j++) {
+                            if ((i % j) == 0) {
+                                esPrimo = false;
+                            }
+                        }
+                        if (esPrimo == true) {
+                            System.out.println("El numero: " + i + " es primo");
+                        }
+
+                    }
+
+                    break;
+                case 4:
+                    System.out.println("Opcion 4");
+                    break;
+                case 5:
+                    System.out.println("Salir");
+                    break;
             }
 
-       }while(opcion < 1 || opcion > 5);
+        } while (opcion < 1 || opcion > 5);
     }
 }
